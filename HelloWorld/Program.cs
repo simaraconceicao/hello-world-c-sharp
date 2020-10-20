de0f2e73f5d20beaebf7b6a5fe033ed57c67bf54
Simara Conceição <contato@simaraconceicao.com>
2020-10-20 01:32:23 -0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nQual seu nome?");
+            var nome = Console.ReadLine();
+            Console.WriteLine($"\nHello {nome}!");
+        }
+    }
+}
